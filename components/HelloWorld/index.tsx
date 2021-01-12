4c9e79491a616d3ef6f167657baa527bb1d766f0
Alexis Cotton <eatingyournoodles@yahoo.com>
2021-01-11 21:11:01 -0600

    Modified homepage text

--- components/HelloWorld/index.tsx
+++ components/HelloWorld/index.tsx
@@ -7,7 +7,7 @@ interface HelloWorldProps {
 
 // This is our custom component
 const HelloWorld = (props: HelloWorldProps) => {
-    return <div>Hello World! {props.name} will take over the world!</div>
+    return <div className='text-5xl text-center'>Hello World! {props.name} will take over the world!</div>
 }
 
 export default HelloWorld
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,12 @@ import HelloWorld from '../components/HelloWorld'
 
 // Define our page with anonymous function
 const IndexPage = () => {
-    return <div><HelloWorld name='Alexis'></HelloWorld><HelloWorld name='Michael'></HelloWorld></div>
+    return <div>
+        <HelloWorld name='Noodle'></HelloWorld>
+        <div className='text-xs text-center text-red-900'>This is a work in progress; check back for changes as I continue to learn!</div>
+        <div>boops boop</div>
+        <div>boop the snoot</div>
+        </div>
 }
 
 
